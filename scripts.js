a5903d3f1d60e55565b0043a94167d7cce15071d
Adam Keevins <adamkeevins@Adams-MacBook-Pro.local>
2020-02-09 13:59:05 -0400

    Backbone Test Files

---
+++ scripts.js
@@ -0,0 +1,64 @@
+// Backbone Model
+
+var Blog = Backbone.Model.extend({
+    defaults: {
+        author: '',
+        title: '',
+        url: ''
+    }
+})
+
+// Backbone Collection
+
+var Blogs = Backbone.Collection.extend({});
+
+//Instantiate new Collection
+
+blogs = new Blogs();
+
+// Backbone View for one blog
+var BlogView = Backbone.View.extend({
+    model: new Blog(),
+    tagName: 'tr',
+    initialize: function() {
+        this.template = _.template($('.blogs-list-template').html())
+    },
+    render: function() {
+        this.$el.html(this.template(this.model.toJSON()))
+        return this;
+    }
+});
+
+// Backbone View for all blogs
+var BlogsView = Backbone.View.extend({
+    model: blogs,
+    el: $('.blogs-list'),
+    initialize: function() {
+        this.model.on('add', this.render, this)
+    },
+    render: function() {
+        var self = this;
+        this.$el.html('');
+        _.each(this.model.toArray(), function(blog){
+            self.$el.append((new BlogView({model: blog})).render().$el)
+        });
+        return this;
+    }
+});
+
+var blogsView = new BlogsView();
+
+$(document).ready(function(){
+    $('.add-blog').on('click', function(){
+        var blog = new Blog({
+            author: $('.author-input').val(),
+            title: $('.title-input').val(),
+            url: $('.url-input').val()
+        })
+        $('.author-input').val('');
+        $('.title-input').val('');
+        $('.url-input').val('');
+        blogs.add(blog);
+    });
+})
+
